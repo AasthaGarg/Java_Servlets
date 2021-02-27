187d845e07134fd8b41c8072e145443fcf4f366a
Aastha garg <bulbul.garg1997@gmail.com>
2021-02-27 19:43:43 +0530

    Create WelcomeServlet.java

---
+++ Assignment12/src/com/WelcomeServlet.java
@@ -0,0 +1,60 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/WelcomeServlet")
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public WelcomeServlet() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    public void init() throws ServletException {
+    	//initialization method
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		out.println("Welcome!!");
+		String name = request.getParameter("name");
+		
+		var ndate= new Date();
+		var hrs= ndate.getHours();
+		if(hrs<12)
+			out.println("Good Morning "+name);
+		else if(hrs<18)
+			out.println("Good Afternoon "+name);
+		else
+			out.println("Good evening "+name);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	public void destroy() {
+		//destroy method
+	}
+
+}
