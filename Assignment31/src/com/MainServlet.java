31e02f6ad4c2d5f9936cb5f39652ff64fd5dccb0
Aastha garg <bulbul.garg1997@gmail.com>
2021-02-27 22:12:13 +0530

    Create MainServlet.java

---
+++ Assignment31/src/com/MainServlet.java
@@ -0,0 +1,48 @@
+package com;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainServlet
+ */
+@WebServlet("/MainServlet")
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MainServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		int number = Integer.valueOf(request.getParameter("num"));
+		
+		if(number<10)
+			response.sendRedirect("Servlet1");
+		else if(number>10 && number<99)
+			response.sendRedirect("Servlet2");
+		else
+			response.sendRedirect("Servlet3");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
