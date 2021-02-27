1507af14daca18c05372cdfcfe98347adbe81d66
Aastha garg <bulbul.garg1997@gmail.com>
2021-02-27 19:51:39 +0530

    Create LoginValidate.java

---
+++ Assignment21/src/com/LoginValidate.java
@@ -0,0 +1,56 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginValidate
+ */
+@WebServlet("/LoginValidate")
+public class LoginValidate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginValidate() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out=response.getWriter();
+		response.setContentType("text/html");
+		
+		String name=request.getParameter("name");
+		String password=request.getParameter("pwd");
+		
+		if(name.equals("john") && password.equals("abc123")) {
+			
+			response.sendRedirect("home.html");
+		}
+		else {
+			out.println("Wrong username or password entered");
+		}
+		
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
