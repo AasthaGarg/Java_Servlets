51a596e0cac90c63760580d4102d0e66c8de00c0
Aastha garg <bulbul.garg1997@gmail.com>
2021-02-27 22:56:50 +0530

    Create Servlet2.java

---
+++ Assignment43/src/com/Servlet2.java
@@ -0,0 +1,54 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet2
+ */
+@WebServlet("/Servlet2")
+public class Servlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		 try{  
+		        response.setContentType("text/html");  
+		        PrintWriter out = response.getWriter();  
+		          
+		        //Getting the value from the hidden field  
+		        String n=request.getParameter("uname");  
+		        out.print("Hello "+n);  
+		  
+		        out.close();  
+		    }catch(Exception e){System.out.println(e);}  
+		    
+	}  
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
