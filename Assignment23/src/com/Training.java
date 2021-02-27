59758851296bc84b3dbfa5e8084df9c427f54bce
Aastha garg <bulbul.garg1997@gmail.com>
2021-02-27 21:47:09 +0530

    Create Training.java

---
+++ Assignment23/src/com/Training.java
@@ -0,0 +1,59 @@
+package com;
+
+public class Training {
+	
+	private int tid;
+	private String tname;
+	private String sdate;
+	private String edate;
+	private String tmode;
+	private String bunit;
+	private String cpersonid;
+	
+	public int getTid() {
+		return tid;
+	}
+	public void setTid(int tid) {
+		this.tid = tid;
+	}
+	public String getTname() {
+		return tname;
+	}
+	public void setTname(String tname) {
+		this.tname = tname;
+	}
+	public String getSdate() {
+		return sdate;
+	}
+	public void setSdate(String sdate) {
+		this.sdate = sdate;
+	}
+	public String getEdate() {
+		return edate;
+	}
+	public void setEdate(String edate) {
+		this.edate = edate;
+	}
+	public String getTmode() {
+		return tmode;
+	}
+	public void setTmode(String tmode) {
+		this.tmode = tmode;
+	}
+	public String getBunit() {
+		return bunit;
+	}
+	public void setBunit(String bunit) {
+		this.bunit = bunit;
+	}
+	public String getCpersonid() {
+		return cpersonid;
+	}
+	public void setCpersonid(String cpersonid) {
+		this.cpersonid = cpersonid;
+	}
+	
+	
+	
+
+}
